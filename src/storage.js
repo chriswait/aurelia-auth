226e0f48b56c7897113d6224cbfc0f8f34313fa6
chriswait <chriswait91@gmail.com>
2016-11-17 11:02:44 +1100

    splits out tests by storage type
    storage doesn't throw when using cookies

--- src/storage.js
+++ src/storage.js
@@ -5,21 +5,39 @@ import {BaseConfig} from './base-config';
 export class Storage {
   constructor(config) {
     this.config = config.current;
-    this.storage = this._getStorage(this.config.storage);
+    this.type = this.config.storage;
+    this.storage = this._getStorage();
+  }
+  isUsingStorage() {
+    return (this.type === 'localStorage' || this.type === 'sessionStorage');
   }
 
-  get(key) { return this.storage.getItem(key); }
-  set(key, value) { return this.storage.setItem(key, value); }
-  remove(key) { return this.storage.removeItem(key); }
-  _getStorage(type) {
-    if (type === 'localStorage') {
+  get(key) {
+    if (this.isUsingStorage()) {
+      return this.storage.getItem(key);
+    }
+  }
+  set(key, value) {
+    if (this.isUsingStorage()) {
+      return this.storage.setItem(key, value);
+    }
+  }
+  remove(key) {
+    if (this.isUsingStorage()) {
+      return this.storage.removeItem(key);
+    }
+  }
+
+  _getStorage() {
+    if (this.type === 'localStorage') {
       if ('localStorage' in window && window.localStorage !== null) return localStorage;
       throw new Error('Local Storage is disabled or unavailable.');
-    } else if (type === 'sessionStorage') {
+    } else if (this.type === 'sessionStorage') {
       if ('sessionStorage' in window && window.sessionStorage !== null) return sessionStorage;
       throw new Error('Session Storage is disabled or unavailable.');
+    } else if (this.type === 'cookies') {
+      return undefined;
     }
-
-    throw new Error('Invalid storage type specified: ' + type);
+    throw new Error('Invalid storage type specified: ' + this.type);
   }
 }
--- test/storage.spec.js
+++ test/storage.spec.js
@@ -4,20 +4,18 @@ import {BaseConfig} from '../src/base-config';
 
 let key = 'testKey';
 let value = 'testValue';
-describe('Storage', () => {
+
+describe('Storage - localStorage', () => {
   it('successfully initialises using defaults (localStorage)', () => {
     let container = new Container();
     let config = new BaseConfig();
     container.registerInstance(BaseConfig, config);
-
     let storage;
     expect(() => {
       storage = container.get(Storage);
     }).not.toThrow();
-
     expect(storage.storage).toBe(localStorage);
   });
-
   it('stores and retrieves values using localStorage', () => {
     let container = new Container();
     let config = new BaseConfig();
@@ -27,7 +25,29 @@ describe('Storage', () => {
     storage.set(key, value);
     expect(storage.get(key)).toBe(value);
   });
+  it('removes items using localStorage', () => {
+    let container = new Container();
+    let config = new BaseConfig();
+    container.registerInstance(BaseConfig, config);
+    let storage = container.get(Storage);
+    storage.set(key, value);
+    storage.remove(key);
+    expect(storage.get(key)).toBe(null);
+  });
+});
 
+describe('Storage - sessionStorage', () => {
+  it('successfully initialises using sessionStorage', () => {
+    let container = new Container();
+    let config = new BaseConfig();
+    config.configure({storage: 'sessionStorage'});
+    container.registerInstance(BaseConfig, config);
+    let storage;
+    expect(() => {
+      storage = container.get(Storage);
+    }).not.toThrow();
+    expect(storage.storage).toBe(sessionStorage);
+  });
   it('stores and retrieves values using sessionStorage', () => {
     let container = new Container();
     let config = new BaseConfig();
@@ -37,30 +57,50 @@ describe('Storage', () => {
     storage.set(key, value);
     expect(storage.get(key)).toBe(value);
   });
-
-  it('removes items', () => {
+  it('removes items using sessionStorage', () => {
     let container = new Container();
     let config = new BaseConfig();
+    config.configure({storage: 'sessionStorage'});
     container.registerInstance(BaseConfig, config);
     let storage = container.get(Storage);
     storage.set(key, value);
     storage.remove(key);
     expect(storage.get(key)).toBe(null);
   });
+});
 
-  it('throws error when trying to initialise using cookies', () => {
+describe('Storage - cookies', () => {
+  it('successfully initialises using cookies', () => {
     let container = new Container();
     let config = new BaseConfig();
     let type = 'cookies'
     config.configure({storage: type});
     container.registerInstance(BaseConfig, config);
-
-    let expectedError = new Error('Invalid storage type specified: ' + type);
-    try {
-      container.get(Storage);
-    } catch (e) {
-      expect(e.innerError).toEqual(expectedError);
-    }
+    let storage;
+    expect(() => {
+      storage = container.get(Storage);
+    }).not.toThrow();
   });
-
+  /*
+  it('stores and retrieves values using cookies', () => {
+    let container = new Container();
+    let config = new BaseConfig();
+    let type = 'cookies'
+    config.configure({storage: type});
+    container.registerInstance(BaseConfig, config);
+    let storage = container.get(Storage);
+    storage.set(key, value);
+    expect(storage.get(key)).toBe(value);
+  });
+  it('removes items using cookies', () => {
+    let container = new container();
+    let config = new baseconfig();
+    config.configure({storage: 'cookies'});
+    container.registerinstance(baseconfig, config);
+    let storage = container.get(storage);
+    storage.set(key, value);
+    storage.remove(key);
+    expect(storage.get(key)).tobe(null);
+  });
+  */
 });
